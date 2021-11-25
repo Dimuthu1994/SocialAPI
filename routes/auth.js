e3808b7bd8f5fc894e08b519c0540e1e2f28525d
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-25 12:25:37 +0530

    hased password

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,20 @@
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 
 // Register
 router.post("/register", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered");
 
+  user = new User(_.pick(req.body, ["username", "email", "password"]));
+  const salt = await bcrypt.genSalt(8);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
   res.send("ok");
 });
