2570055394385cb2bade47379fbca63c74a872ce
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-26 09:12:37 +0530

    update a post

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,20 @@ router.post("/", async (req, res) => {
   res.send("saved to database");
 });
 //update a post
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const post = await Post.findById(req.params.id);
+  if (!post) return res.status(404).send("The post with given id not found");
+
+  if (post.userId === req.body.userId) {
+    await post.updateOne({ $set: req.body });
+    res.send("the post has updated");
+  } else {
+    res.status(403).send("you can update only your post");
+  }
+});
 //delete a post
 //like a post
 // get a post
