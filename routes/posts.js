f25cd37a0a300dc54a9eedbaccc5415accdb46e4
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-26 09:44:28 +0530

    post like

--- routes/posts.js
+++ routes/posts.js
@@ -41,6 +41,20 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //like a post
+router.put("/:id/like", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const post = await Post.findById(req.params.id);
+  if (!post) return res.status(404).send("The post with given id not found");
+
+  if (!post.likes.includes(req.body.userId)) {
+    await post.updateOne({ $push: { likes: req.body.userId } });
+    res.send("post liked");
+  } else {
+    await post.updateOne({ $pull: { likes: req.body.userId } });
+    res.send("post disliked");
+  }
+});
 // get a post
 // get timeline posts
 
