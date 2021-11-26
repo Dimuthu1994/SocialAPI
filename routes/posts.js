1cf4c31e6984a1d30addf13f34ed582f1618619e
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-26 09:23:52 +0530

    delete post

--- routes/posts.js
+++ routes/posts.js
@@ -28,6 +28,8 @@ router.put("/:id", async (req, res) => {
 });
 //delete a post
 router.delete("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
   const post = await Post.findById(req.params.id);
   if (!post) return res.status(404).send("The post with given id not found");
 
